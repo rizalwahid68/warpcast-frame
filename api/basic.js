b4aec08e05419dc8614de5f4ecfbe08b46eadda3
Muhammad Rizal Nur Wakhid <82570585+rizalwahid68@users.noreply.github.com>
2024-06-18 22:33:18 +0700

    Delete api directory

--- api/basic.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  if (req.method === 'POST') {
-    res.status(200).json({ message: 'Request received' });
-  } else {
-    res.status(404).json({ error: 'Not Found' });
-  }
-};
